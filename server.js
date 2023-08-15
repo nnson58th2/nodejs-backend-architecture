714ead53bb50171a530a51e77624c6edc22dbb42
Son Nguyen <nhuson2306@gmail.com>
2023-08-15 21:47:08 +0700

    Initital project

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require('./src/app');
+
+const PORT = 3055;
+
+const server = app.listen(PORT, () => {
+  console.log(`Web Service eCommerce start with ${PORT}`);
+});
+
+process.on('SIGINT', () => {
+  server.close(() => console.log(` Exist server express`));
+});
---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+
+const app = express();
+
+// Init Middlewares
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+
+// Init DB
+
+// Init routes
+
+// Handling error
+
+module.exports = app;
