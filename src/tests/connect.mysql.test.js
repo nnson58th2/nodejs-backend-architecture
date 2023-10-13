9f70a649622e75eb7b96c33843a593a13e251b30
Son Nguyen <nhuson2306@gmail.com>
2023-10-13 22:26:35 +0700

    Section 56: Thực hành tự tạo dữ liệu test 10.000.000 records trong MySQL

--- src/tests/connect.mysql.test.js
+++ src/tests/connect.mysql.test.js
@@ -10,6 +10,45 @@ const pool = mysql.createPool({
     database: 'shopDEV',
 });
 
+const batchSize = 100_000; // adjust batch size
+const totalSize = 1_000_000; // adjust total size
+let currentId = 1;
+
+console.time('-----TIMER-----');
+const insertBatch = async () => {
+    const values = [];
+
+    for (let i = 0; i < batchSize && currentId <= totalSize; i++) {
+        const name = `name-${currentId}`;
+        const age = currentId;
+        const address = `address-${currentId}`;
+        values.push([currentId, name, age, address]);
+        currentId++;
+    }
+
+    if (!values.length) {
+        console.timeEnd('-----TIMER-----');
+        pool.end((err) => {
+            if (err) {
+                console.log(`Error occurred while running batch`);
+            } else {
+                console.log(`Connection pool closed successfully`);
+            }
+        });
+        return;
+    }
+
+    const sql = `INSERT INTO test_table (id, name, age, address) VALUES ?`;
+
+    pool.query(sql, [values], async (error, results, fields) => {
+        if (error) throw error;
+        console.log(`Inserted ${results.affectedRows} records`);
+        await insertBatch();
+    });
+};
+
+insertBatch().catch((error) => console.error(`Error::${error}`));
+
 // Perform a sample operation
 // pool.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
 //     if (error) throw error;
@@ -22,13 +61,13 @@ const pool = mysql.createPool({
 //     });
 // });
 
-pool.query('SELECT * from users', (error, results, fields) => {
-    if (error) throw error;
-    console.log(`Query result`, results);
+// pool.query('SELECT * from users', (error, results, fields) => {
+//     if (error) throw error;
+//     console.log(`Query result`, results);
 
-    // Close pool connection
-    pool.end((error) => {
-        if (error) throw error;
-        console.log('Connection closed');
-    });
-});
+//     // Close pool connection
+//     pool.end((error) => {
+//         if (error) throw error;
+//         console.log('Connection closed');
+//     });
+// });
